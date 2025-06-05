bfe55a91f4f65200b35823d6268e33cb8590f13e
Sgal Cheung <zhangsigao@live.com>
2025-06-05 13:57:43 +0800

    Add .assetsignore and wrangler configuration for astro-readium project
    - Created .assetsignore file to exclude specific assets from processing.
    - Added wrangler.jsonc configuration file to set up project metadata, asset binding, and observability settings.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,27 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+
   vite: {
+    // reference: https://github.com/withastro/astro/issues/12824
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD
+        ? {
+            "react-dom/server": "react-dom/server.edge",
+          }
+        : undefined,
+    },
     ssr: {
       noExternal: ["@edrlab/thorium-web"],
     },
   },
+
+  adapter: cloudflare(),
 });
